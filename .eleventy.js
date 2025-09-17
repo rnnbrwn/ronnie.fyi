a46fe4aed536376a4a86ff8e0fc49da0a164bb53
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2025-09-18 00:17:36 +0100

    Add automatic post ordering by file creation time + new blog post
    - Add custom posts collection that sorts by file birthtime/creation time
    - Eliminates need for manual timestamps when posts have same date
    - Posts now automatically appear in chronological order based on when files were created
    - Works seamlessly with existing head(-6) and reverse template logic
    - Most recently created posts appear first on home page
    
    New post: 'World War 2 is my Roman Empire' - thoughts on diving deep into WW2 movies and content

--- .eleventy.js
+++ .eleventy.js
@@ -77,6 +77,20 @@ module.exports = function (eleventyConfig) {
         return filterTagList([...tagSet]);
     });
 
+    // Create posts collection sorted by file creation time
+    eleventyConfig.addCollection("posts", function (collection) {
+        const fs = require("fs");
+        return collection.getFilteredByTag("posts").sort(function (a, b) {
+            // Get file creation times (birthtime) or fall back to modification time
+            const aTime = fs.statSync(a.inputPath).birthtime || fs.statSync(a.inputPath).mtime;
+            const bTime = fs.statSync(b.inputPath).birthtime || fs.statSync(b.inputPath).mtime;
+            
+            // Sort by creation time (oldest first, so newest end up at the end)
+            // This works with head(-6) and | reverse to show newest first
+            return aTime - bTime;
+        });
+    });
+
     // Copy the `img` and `css` folders to the output
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css");
