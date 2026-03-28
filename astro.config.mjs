f497dda08d340981e8c4ece8324c6e6d233c81b9
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2026-03-28 12:38:05 +0000

    remove references to mdx as I'm not using it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import remarkYouTube from './src/plugins/remark-youtube.mjs';
+import rehypeRaw from 'rehype-raw';
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkYouTube],
+  markdown: {
+      remarkPlugins: [remarkYouTube],
+      rehypePlugins: [rehypeRaw],
 	},
 });
