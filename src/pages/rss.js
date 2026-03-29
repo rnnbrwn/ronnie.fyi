3cd68b2bed60a0d55a93f0204ed22efb368cd5eb
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2026-03-29 00:48:07 +0000

    rss images

--- src/pages/rss.js
+++ src/pages/rss.js
@@ -9,11 +9,23 @@ export async function GET(context) {
 		title: 'ronnie.fyi',
 		description: 'Writing about music, technology, and whatever else.',
 		site: context.site,
-		items: sorted.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			link: `/posts/${post.id}/`,
-		})),
+		xmlns: {
+			media: 'http://search.yahoo.com/mrss/',
+		},
+		items: sorted.map((post) => {
+			const imageUrl = post.data.image
+				? new URL(post.data.image.url, context.site).href
+				: null;
+
+			return {
+				title: post.data.title,
+				pubDate: post.data.pubDate,
+				description: post.data.description,
+				link: `/posts/${post.id}/`,
+				customData: imageUrl
+					? `<media:content url="${imageUrl}" medium="image" />`
+					: '',
+			};
+		}),
 	});
 }
