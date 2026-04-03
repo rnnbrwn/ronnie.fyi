42b8189bdbd2e952e8c5ae43e0bb7f836871023e
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2026-04-03 13:55:38 +0100

    setup sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import remarkYouTube from './src/plugins/remark-youtube.mjs';
 import rehypeRaw from 'rehype-raw';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ronnie.fyi',
   trailingSlash: 'never',
+
   markdown: {
       remarkPlugins: [remarkYouTube],
       rehypePlugins: [rehypeRaw],
-	},
+    },
+
+  integrations: [sitemap()],
 });
