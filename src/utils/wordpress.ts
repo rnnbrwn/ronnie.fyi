897d29a3f80040c01cc0b3c312e9081362352880
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2026-04-18 21:08:37 +0100

    some styles and initial WP hook up

---
+++ src/utils/wordpress.ts
@@ -0,0 +1,24 @@
+const WORDPRESS_API_URL = import.meta.env.WORDPRESS_API_URL ?? 'https://cms.ronnie.fyi/graphql';
+
+async function fetchGraphQL(query: string, variables = {}) {
+	const response = await fetch(WORDPRESS_API_URL, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ query, variables }),
+	});
+	const { data } = await response.json();
+	return data;
+}
+
+export async function getPage(slug: string) {
+	const data = await fetchGraphQL(
+		`query GetPage($slug: ID!) {
+			page(id: $slug, idType: URI) {
+				title
+				content
+			}
+		}`,
+		{ slug }
+	);
+	return data?.page ?? null;
+}
